e0b9a7aa752be4bc3f70d37a8b75d3cf35e2352e
clark21 <clark@dev-engine.net>
2015-10-06 16:27:52 +0800

    test suite

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,7 +6,10 @@
  * Copyright and license information can be found at LICENSE
  * distributed with this package.
  */
-ob_start();
-//assuming we are in a composer vendor folder
-require_once __DIR__.'/../../../autoload.php';
-require_once __DIR__.'/../../core/src/Control.php';
+
+if(file_exists(__DIR__.'/../../../autoload.php')) {
+    require_once __DIR__.'/../../../autoload.php';
+} else {
+    require_once __DIR__.'/../vendor/autoload.php';
+}
+
