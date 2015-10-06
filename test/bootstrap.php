9444b21e4d60283f40db28b654631725d4fec3a8
clark21 <clark@dev-engine.net>
2015-10-06 16:36:50 +0800

    test suite

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,7 +6,7 @@
  * Copyright and license information can be found at LICENSE
  * distributed with this package.
  */
-
+ob_start();
 if(file_exists(__DIR__.'/../../../autoload.php')) {
     require_once __DIR__.'/../../../autoload.php';
 } else {
